58b7a6e5bc11f6d1b4c1c31430659557f8fe4dd9
Vikram B Rojo <atav1k@users.noreply.github.com>
2020-01-01 14:58:00 -0700

    Initial commit

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,26 @@
+<script>
+  import Header from "../components/Header.svelte";
+
+  export let segment;
+</script>
+
+<style>
+
+</style>
+
+<div class="viewHt100 flex flexCol flexBetween">
+  <Header {segment} />
+
+  <main class="padY4">
+    <slot />
+  </main>
+
+  <footer class="wid100">
+    <nav class="nav cols">
+      <div class="navLeft col">
+        <a href="https://github.com/atav1k/sapper-fortune-template">Github</a>
+        <a href="/">&copy; {new Date().getFullYear()}</a>
+      </div>
+    </nav>
+  </footer>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script>
+
+</script>
+
+<style>
+
+</style>
+
+<svelte:head>
+  <title>Sapper Fortune Template</title>
+</svelte:head>
+
+<article class="cols">
+  <div class="col">
+    <h1>Welcome to your Sapper site.</h1>
+  </div>
+</article>
